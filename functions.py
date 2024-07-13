d529c86eef35fd0e4686587a4aba80882f693a7a
canhackers <linulinu@naver.com>
2024-07-13 10:03:33 +0900

    fix auto recovery

--- functions.py
+++ functions.py
@@ -1,14 +1,15 @@
 import os
-import time
 import json
 
 json_file = os.path.join('/home/jupiter_settings.json')
-def initialize_canbus_connection(delay=0):
+def initialize_canbus_connection():
     try:
+        os.system('sudo modprobe -r mcp251x')
+        os.system('sudo modprobe mcp251x')
         os.system('sudo ip link set can0 type can bitrate 500000')
         os.system('sudo ifconfig can0 down')
-        time.sleep(delay)
         os.system('sudo ifconfig can0 up')
+        print('can bus initialized')
         return True
     except Exception as e:
         print('CAN Bus Initialize Error', e)
