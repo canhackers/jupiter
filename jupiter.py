dac65bc5b418c5e274ca9c3240d103273abe3418
canhackers <linulinu@naver.com>
2024-08-24 11:18:41 +0900

    Threading 반영

--- jupiter.py
+++ jupiter.py
@@ -253,7 +253,7 @@ class Hud(threading.Thread):
         last_update_slow = 0
         while self.thread_online:
             if not self.NAVDY.connected:
-                self.connector.conected.wait()
+                self.connector.connected.wait()
 
             time.sleep(0.2)
             current_time = DASH.current_time
