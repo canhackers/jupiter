a46742241af5f7077e92e45b652da9db5ca30587
canhackers <linulinu@naver.com>
2024-09-04 19:48:21 +0900

    버튼 관련 코드 리팩토링

--- tesla.py
+++ tesla.py
@@ -436,7 +436,6 @@ class Autopilot:
         self.speed_deque.popleft()
         self.speed_deque.append(self.dash.ui_speed)
         self.smooth_speed = sum(s for s in self.speed_deque) / 3
-        print('auto pilot status', self.auto_distance, self.manual_distance, self.autosteer)
         if self.auto_distance and not self.manual_distance and self.autosteer:
             if self.smooth_speed <= 60:
                 self.distance_target = 3
@@ -446,7 +445,6 @@ class Autopilot:
                 self.distance_target = 5
             else:
                 self.distance_target = 6
-            print(self.smooth_speed, self.distance_target)
             self.set_distance(self.distance_target)
 
         # Mars Mode from Spleck's github (https://github.com/spleck/panda)
