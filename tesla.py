35d5e72d80d9e7cd63cd4b62b5c7d588440bb6e7
canhackers <linulinu@naver.com>
2024-09-04 19:39:49 +0900

    버튼 관련 코드 리팩토링

--- tesla.py
+++ tesla.py
@@ -436,8 +436,8 @@ class Autopilot:
         self.speed_deque.popleft()
         self.speed_deque.append(self.dash.ui_speed)
         self.smooth_speed = sum(s for s in self.speed_deque) / 3
+        print('auto pilot status', self.auto_distancem, self.manual_distance, self.autosteer)
         if self.auto_distance and not self.manual_distance and self.autosteer:
-
             if self.smooth_speed <= 60:
                 self.distance_target = 3
             elif self.smooth_speed <= 80:
